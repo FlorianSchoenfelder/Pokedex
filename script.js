9b2f269e8c8a83715fc4ec56c2845d07e50063c7
FlorianSchoenfelder <f.schoenfelder22@gmail.com>
2023-11-13 22:18:48 +0100

    added pokemon id color, and some improvements

--- script.js
+++ script.js
@@ -26,32 +26,83 @@ async function renderPokemonUrls() {
         const url = links[j];
         let response = await fetch(url);
         let pokemonData = await response.json();
+        let type1 = pokemonData['types']['0']['type']['name'];
+        
         let pokemonName = pokemonData['name'].charAt(0).toUpperCase() + pokemonData['name'].slice(1)
         console.log(pokemonData);
-        document.getElementById('mainContainer').innerHTML+= /*html*/`
-            <div class="pokemon">
+
+        document.getElementById('mainContainer').innerHTML += /*html*/`
+            <div class="pokemon" id="pokemon_${j}">
                 <div class="information">
                     <div class="name">${pokemonName}</div>
-                <div class="mainInformation">
-                    <span>
-                        #${pokemonData['id']} <br>
-                        ${pokemonData['weight']}Kg
-                        
-                    </span>
+                    <div class="mainInformation">
+                        <span>
+                            #${pokemonData['id']} <br>
+                            ${pokemonData['weight']}Kg <br>
+                            ${type1}
+                        </span>
+                    </div>
                 </div>
+                <div class="pokemonImageContainer">
+                    <img class="pokemonImage" src="${pokemonData['sprites']['other']['official-artwork']['front_default']}">
+                </div>
+                
             </div>
-                
-                
-                <img src="${pokemonData['sprites']['front_default']}" alt="">
-            </div>  
         `;
+
+        await selectBackground(type1, j);
     }
+
     document.getElementById('mainContainer').innerHTML += /*html*/`
-        <button class="moreButton" onclick="loadMorePokemons()">Load More</button>
+        <div class="buttonContainer">
+            <button class="moreButton" onclick="loadMorePokemons()">Load More</button>
+        </div>
+        
     `;
 }
 
 function loadMorePokemons() {
     loadBeginning += 30;
     init();
+}
+
+function selectBackground(type1, index) {
+    let pokemonElement = document.getElementById('pokemon_' + index);
+
+    if (type1 == 'grass') {
+        pokemonElement.classList.add('grassBackground');
+    }
+    if (type1 == 'fire') {
+        pokemonElement.classList.add('fireBackground');
+    }
+    if (type1 == 'water') {
+        pokemonElement.classList.add('waterBackground');
+    }
+    if (type1 == 'bug') {
+        pokemonElement.classList.add('bugBackground');
+    }
+    if (type1 == 'normal') {
+        pokemonElement.classList.add('normalBackground');
+    }
+    if (type1 == 'poison') {
+        pokemonElement.classList.add('poisonBackground');
+    }
+    if (type1 == 'electric') {
+        pokemonElement.classList.add('electricBackground');
+    }
+    if (type1 == 'ground') {
+        pokemonElement.classList.add('groundBackground');
+    }
+    if (type1 == 'fairy') {
+        pokemonElement.classList.add('fairyBackground');
+    }
+    if (type1 == 'psychic') {
+        pokemonElement.classList.add('psychicBackground');
+    }
+    if (type1 == 'rock') {
+        pokemonElement.classList.add('rockBackground');
+    }
+    if (type1 == 'ghost') {
+        pokemonElement.classList.add('ghostBackground');
+    }
 }
