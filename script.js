d94fd05bc2abb7353f92eb453fa314593c672e69
FlorianSchoenfelder <f.schoenfelder22@gmail.com>
2023-11-17 20:08:09 +0100

    first detailed card layout

--- script.js
+++ script.js
@@ -12,27 +12,32 @@ async function loadpokemonApi() {
     let response = await fetch(url);
     let responseAsJson = await response.json();
 
-    console.log(responseAsJson['results']);
+    // console.log(responseAsJson['results']);
 
     for (let i = 0; i < responseAsJson['results'].length; i++) {
         const element = responseAsJson['results'][i];
         links.push(element['url']); // Speichere nur die URL in der links-Liste
-    }    
+    }
 }
 
 async function renderPokemonUrls() {
+    document.getElementById('mainContainer').classList.add('animateIn');
+    document.getElementById('showPokemon').classList.add('d-none');
+    document.getElementById('mainContainer').classList.remove('d-none');
+    document.getElementById('backgroundContainer').classList.remove('d-none');
+    document.getElementById('showPokemon').innerHTML = '';
     document.getElementById('mainContainer').innerHTML = '';
     for (let j = 0; j < links.length; j++) {
         const url = links[j];
         let response = await fetch(url);
         let pokemonData = await response.json();
         let type1 = pokemonData['types']['0']['type']['name'];
-        
-        let pokemonName = pokemonData['name'].charAt(0).toUpperCase() + pokemonData['name'].slice(1)
-        console.log(pokemonData);
+
+        let pokemonName = pokemonData['name'].charAt(0).toUpperCase() + pokemonData['name'].slice(1);
+        // console.log(pokemonData);
 
         document.getElementById('mainContainer').innerHTML += /*html*/`
-            <div class="pokemon" id="pokemon_${j}">
+            <div class="pokemon" id="pokemon_${j}" onclick="showPokemon(${j})">
                 <div class="information">
                     <div class="name">${pokemonName}</div>
                     <div class="mainInformation">
@@ -64,6 +69,67 @@ async function loadMorePokemons() {
     document.getElementById('mainContainer').innerHTML += '';
     await loadpokemonApi();
     await renderPokemonUrls();
+}
+
+async function showPokemon(j) {
+    document.getElementById('showPokemon').classList.remove('d-none');
+    document.getElementById('showPokemon').classList.add('animateIn');
+    document.getElementById('mainContainer').classList.add('d-none');
+    document.getElementById('backgroundContainer').classList.add('d-none');
+
+    let url = links[j];
+    let response = await fetch(url);
+    let pokemon = await response.json();
+    let type1 = pokemon['types']['0']['type']['name'];
+    console.log(type1);
+    console.log(pokemon);
+    let number = `${pokemon['id']}`;
+    console.log(number);
+    let paddedNumber = number.padStart(3, "0");
+    let pokemonName = pokemon['name'].charAt(0).toUpperCase() + pokemon['name'].slice(1);
+
+
+    let pokemoncard = document.getElementById('showPokemon');
+    pokemoncard.innerHTML += /*html*/`
+        <div class="pokemonBackground" id="pokemon_${j}" onclick="renderPokemonUrls()">
+            <div class="pokemonCard" id="pokemonCard" onclick="stopPropagation(event)">
+                <div class="cardIdNumber">
+                    #${paddedNumber}
+                </div>
+
+                <div class="cardInformation">
+                    <div class="aboutSection"> <!--about -->
+                        <div class="cardName"> <!-- Name-->
+                            ${pokemonName}
+                        </div>
+                        <div class="bodyInformation"> <!--Gewicht etc. -->
+                            <div> <!-- Rechts-->
+                            Height <br>
+                            Weight <br>
+                            Abilities
+                            </div>
+                            <div> <!-- Links-->
+                            ${pokemon['height']} <br>
+                            ${pokemon['weight']} <br> 
+                            ${pokemon['abilities']['0']['ability']['name']}
+                            </div>
+                        </div>
+                    </div> 
+                    <div class="cardImagePosition"> <!-- IMG-->
+                        <img class="cardImage" src="${pokemon['sprites']['other']['dream_world']['front_default']}">
+                    </div> 
+                    
+                </div>                
+                <!-- ${pokemon['name']}
+                ${pokemon['height']} -->
+            </div>
+        </div>
+    `;
+    await selectBackground(type1, j);
+}
+
+function stopPropagation(event) {
+    event.stopPropagation();
 }
 
 function selectBackground(type1, index) {
